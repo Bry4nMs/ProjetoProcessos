1011e20a9ec3628dabaa23ef12e7012a9dedeedc
Bry4Ms <bryanmds_1@hotmail.com>
2025-10-01 14:53:24 -0300

    01/10

--- add_nome_acao_column.sql
+++
@@ -1,8 +0,0 @@
--- Comando SQL para adicionar a coluna 'nome_acao' à tabela 'processes'
--- Execute este comando no seu banco de dados Supabase
-
-ALTER TABLE public.processes 
-ADD COLUMN nome_acao character varying;
- 
--- Comentário opcional para documentar a coluna
-COMMENT ON COLUMN public.processes.nome_acao IS 'Nome da ação do processo'; 
